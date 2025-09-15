a49ebeeda771a04e5b7972c086d63af140d7c21c
taehyun <tyoonn53@gmail.com>
2025-09-15 18:04:29 -0500

    added variable c

--- calculator.py
+++ calculator.py
@@ -1,8 +1,8 @@
 # calculator.py
 # A simple calculator for practicing Git commands
 
-def add(a, b):
-    return a + b
+def add(a, b, c):
+    return a + b + c
 
 def subtract(a, b):
     return a - b
